aab56aeb1f1053e3a25a524c7eb159e9693649dc
Ashish jha <113693962+Ashjha75@users.noreply.github.com>
2023-04-11 15:36:01 +0530

    Add files via upload

--- src/app/authentications/authentications.module.ts
+++ src/app/authentications/authentications.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthenticationsRoutingModule } from './authentications-routing.module';
+import { RegistrationComponent } from './registration/registration.component';
+import { VerificationsComponent } from './verifications/verifications.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [RegistrationComponent, VerificationsComponent],
   imports: [
     CommonModule,
     AuthenticationsRoutingModule
-  ]
+  ],
+  exports :[RegistrationComponent, VerificationsComponent]
 })
 export class AuthenticationsModule { }
